e253992c0535b53b9935fc618ffb3a9f18232b83
Yunsu <rladbstn1212@gmail.com>
2026-09-07 14:03:35 +0900

    파일 변경

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -170,7 +170,7 @@ export default function Navbar() {
             게시판
           </NavLink>
           <a
-            href="/QA_Copy.zip?v=1.0.0"
+            href="/QA_Copy.zip?v=1.0.1"
             download
             style={{
               padding: "6px 14px",
